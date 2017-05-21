1807ba1b90faf9ef057a86ab977b7b085f22da09
TarasVasylenko <t.a.vasylenko@gmail.com>
2017-05-21 15:57:50 +0300

    bouncy 3 sections

---
+++ js/main.js
@@ -0,0 +1,35 @@
+$('.menu').on('click', 'a', function(event){
+	event.preventDefault();
+	var link = $(this).attr('href');
+	//console.log(link);
+	var distanse = $(link).offset().top;
+	$('html, body').animate({
+		scrollTop:  distanse
+	}, 700);
+});
+
+// izotope
+var $grid = $('.grid').isotope({
+  itemSelector: '.grid-item',
+  layoutMode: 'masonry',
+  masonry: {
+    horizontalOrder: true
+  }
+})
+
+// filters izotope
+$('.filter-button-group').on( 'click', 'button', function() {
+  var filterValue = $(this).attr('data-filter');
+  //console.log(filterValue);
+  $grid.isotope({ filter: filterValue });
+});
+
+$(document).ready(function(){
+  $('.slider').slick({
+  infinite: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  dots: true
+  });
+});
+
